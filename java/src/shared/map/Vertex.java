af622ea64b6c82edc628fac17ae068ae83d56ba0
CaptBoscho <corbin.byers@gmail.com>
2016-01-16 15:47:37 -0700

    hex, edge and vertex

--- java/src/shared/map/Vertex.java
+++ java/src/shared/map/Vertex.java
@@ -1,7 +1,43 @@
 package shared.map;
 
+import shared.locations.VertexLocation;
+
 /**
  * Created by Corbin on 1/16/2016.
  */
 public class Vertex {
+
+    private VertexLocation vertexLoc;
+    //private Building building;
+    //private Port port;
+
+    public Vertex(VertexLocation loc){
+        vertexLoc = loc;
+        //building = null;
+        //port = null;
+    }
+
+    /**
+     *
+     * @return
+     */
+    /*public Building existsBuilding(){
+
+    }*/
+
+    /**
+     *
+     * @return
+     */
+    public boolean canAddBuilding(){
+        return true;
+    }
+
+    /*public void setBuilding(Building b){
+
+    }*/
+    /*
+    public void setPort(Port p){
+
+    }*/
 }
