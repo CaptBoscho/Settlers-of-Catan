f3db370801bb1459debae40d8a30798d3ae78d2f
CaptBoscho <corbin.byers@gmail.com>
2016-01-21 21:40:20 -0700

    rough Facade class, as well as JavaDoc updates for edge and vertex

---
+++ java/src/shared/facade/Facade.java
@@ -0,0 +1,79 @@
+package shared.facade;
+import shared.*;
+
+/**
+ * The Facade class handles all the communication
+ * between the UI and game model.
+ */
+public class Facade {
+
+    private Map map;
+    private Game game;
+
+    /**
+     * Asks the game who then asks the turn tracker
+     * if it's the player's turn.
+     * @param playerID
+     */
+    public boolean myTurn(int playerID){
+        return true;
+    }
+
+    /**
+     * Facade asks if it's the player's turn, then checks the players
+     * hand to see if they have enough resources, then asks the map
+     * class if that player can build an edge at that location.
+     * @param playerID
+     * @param edge
+     * @return
+     */
+    public boolean canBuildRoad(int playerID, EdgeLocation edge){
+        return true;
+    }
+
+    /**
+     * Facade asks if it's the player's turn, then checks the players
+     * hand to see if they have enough resources, then asks the map
+     * class if that player can build a Building at that location.
+     * @param playerID
+     * @param vertex
+     * @return
+     */
+    public boolean canBuildBuilding(int playerID, VertexLocation vertex){
+        return true;
+    }
+
+    /**
+     * Facade asks if it's the player's turn, then checks the players
+     * hand to see if they have enough resources to buy a development
+     * card.
+     * @param playerID
+     * @return
+     */
+    public boolean canBuyDC(int playerID){
+        return true;
+    }
+
+    /**
+     * Facade asks the game who then asks the turn tracker if trading
+     * is permitted for this player.
+     * @param playerID
+     * @return
+     */
+    public boolean canTrade(int playerID){
+        return true;
+    }
+
+    /**
+     * Facade asks the game who then asks the turn tracker if the
+     * player can play a Development CArd
+     * @param playerID
+     * @return
+     */
+    public boolean canPlayDC(int playerID){
+        return true;
+    }
+
+    
+
+}
