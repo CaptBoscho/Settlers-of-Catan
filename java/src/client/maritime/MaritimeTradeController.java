9530f5c3b88f249b9632e55945af7f0623aa8bb7
Kyle Cornelison <cornelison.kylejohn@gmail.com>
2016-02-11 20:08:23 -0700

    Phase 2 Design
    Adding the Design Doc for Phase 2 and some proof for the Observer
    Pattern.

--- java/src/client/maritime/MaritimeTradeController.java
+++ java/src/client/maritime/MaritimeTradeController.java
@@ -3,11 +3,14 @@ package client.maritime;
 import shared.definitions.*;
 import client.base.*;
 
+import java.util.Observable;
+import java.util.Observer;
+
 
 /**
  * Implementation for the maritime trade controller
  */
-public class MaritimeTradeController extends Controller implements IMaritimeTradeController {
+public class MaritimeTradeController extends Controller implements IMaritimeTradeController, Observer {
 
 	private IMaritimeTradeOverlay tradeOverlay;
 	
@@ -60,6 +63,20 @@ public class MaritimeTradeController extends Controller implements IMaritimeTrad
 
 	@Override
 	public void unsetGiveValue() {
+
+	}
+
+	/**
+	 * This method is called whenever the observed object is changed. An
+	 * application calls an <tt>Observable</tt> object's
+	 * <code>notifyObservers</code> method to have all the object's
+	 * observers notified of the change.
+	 *
+	 * @param o   the observable object.
+	 * @param arg an argument passed to the <code>notifyObservers</code>
+	 */
+	@Override
+	public void update(Observable o, Object arg) {
 
 	}
 }
