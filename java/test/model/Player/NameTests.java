0765ed06bdba7baa86cc6994572e8bcc69039dd8
Derek Argueta <darguetap@gmail.com>
2016-02-12 18:23:09 -0700

    add tests for name

--- java/test/model/Player/NameTests.java
+++ java/test/model/Player/NameTests.java
@@ -27,4 +27,35 @@ public class NameTests {
         assertTrue(one.equals(two));
         assertFalse(one.equals(three));
     }
+
+    @Test
+    public void testInvalidNames() {
+        try {
+            new Name("ValidName");
+            assertTrue(true);
+        } catch (InvalidNameException e) {
+            assertFalse(true);
+        }
+
+        try {
+            new Name("Not a Name");
+            assertTrue(false);
+        } catch (InvalidNameException e) {
+            assertTrue(true);
+        }
+
+        try {
+            new Name("numb3rs");
+            assertTrue(false);
+        } catch (InvalidNameException e) {
+            assertTrue(true);
+        }
+
+        try {
+            new Name("Derek!!!!");
+            assertTrue(false);
+        } catch (InvalidNameException e) {
+            assertTrue(true);
+        }
+    }
 }
