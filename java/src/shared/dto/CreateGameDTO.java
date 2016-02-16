2809e21edcc439db87fa2f919c04fbb98838fc51
Derek Argueta <darguetap@gmail.com>
2016-02-16 03:11:10 -0700

    progress on join game controller

--- java/src/shared/dto/CreateGameDTO.java
+++ java/src/shared/dto/CreateGameDTO.java
@@ -10,22 +10,22 @@ import shared.model.JsonSerializable;
  */
 public final class CreateGameDTO implements JsonSerializable{
 
-    private boolean randomTiles;
+    private boolean randomHexes;
     private boolean randomNumbers;
     private boolean randomPorts;
     private String name;
 
-    public CreateGameDTO(boolean rt, boolean rn, boolean rp, String n) {
+    public CreateGameDTO(boolean randomHexes, boolean rn, boolean rp, String n) {
         assert n != null;
 
-        this.randomTiles = rt;
+        this.randomHexes = randomHexes;
         this.randomNumbers = rn;
         this.randomPorts = rp;
         this.name = n;
     }
 
     public boolean isRandomTiles() {
-        return this.randomTiles;
+        return this.randomHexes;
     }
 
     public boolean isRandomNumbers() {
@@ -48,7 +48,7 @@ public final class CreateGameDTO implements JsonSerializable{
     @Override
     public JsonObject toJSON() {
         JsonObject obj = new JsonObject();
-        obj.addProperty("randomTiles", this.randomTiles);
+        obj.addProperty("randomTiles", this.randomHexes);
         obj.addProperty("randomNumbers", this.randomNumbers);
         obj.addProperty("randomPorts", this.randomPorts);
         obj.addProperty("name", this.name);
