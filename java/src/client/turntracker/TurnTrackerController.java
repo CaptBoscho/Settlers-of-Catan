71235dbe9f2cb6557b398ec3aeca1d2cc9aeebbb
Kyle Cornelison <cornelison.kylejohn@gmail.com>
2016-02-16 19:22:26 -0700

    Turn
    Work on Turn Controller

--- java/src/client/turntracker/TurnTrackerController.java
+++ java/src/client/turntracker/TurnTrackerController.java
@@ -1,16 +1,27 @@
 package client.turntracker;
 
+import client.facade.Facade;
+import client.services.UserCookie;
+import org.apache.http.cookie.Cookie;
 import shared.definitions.CatanColor;
 import client.base.*;
+import shared.exceptions.PlayerExistsException;
+
+import java.util.Observable;
+import java.util.Observer;
 
 
 /**
  * Implementation for the turn tracker controller
  */
-public class TurnTrackerController extends Controller implements ITurnTrackerController {
+public class TurnTrackerController extends Controller implements ITurnTrackerController, Observer {
+	private Facade facade;
+	private int id;
 
 	public TurnTrackerController(ITurnTrackerView view) {
 		super(view);
+		facade = Facade.getInstance();
+		id = UserCookie.getInstance().getPlayerId();
 		initFromModel();
 	}
 	
@@ -25,8 +36,28 @@ public class TurnTrackerController extends Controller implements ITurnTrackerCon
 	}
 	
 	private void initFromModel() {
+		try {
+			CatanColor currentPlayerColor = facade.getPlayerColorByID(id);
+		} catch (PlayerExistsException e) {
+			e.printStackTrace();
+		}
+
 		//<temp>
-		getView().setLocalPlayerColor(CatanColor.RED);
+		getView().setLocalPlayerColor();
 		//</temp>
+	}
+
+	/**
+	 * This method is called whenever the observed object is changed. An
+	 * application calls an <tt>Observable</tt> object's
+	 * <code>notifyObservers</code> method to have all the object's
+	 * observers notified of the change.
+	 *
+	 * @param o   the observable object.
+	 * @param arg an argument passed to the <code>notifyObservers</code>
+	 */
+	@Override
+	public void update(Observable o, Object arg) {
+		initFromModel();
 	}
 }
