27f310ec26b16b9559b7c628eaa336bc822fe31c
Derek Argueta <darguetap@gmail.com>
2016-03-09 16:28:35 -0700

    start on server

---
+++ java/src/server/controllers/UserController.java
@@ -0,0 +1,11 @@
+package server.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Derek Argueta
+ */
+@RestController
+public class UserController {
+    // -- TODO
+}
