84307783d610ac9ff17d54082ce2187fc8724ac5
Derek Argueta <darguetap@gmail.com>
2016-03-10 23:29:34 -0700

    server twerking

--- java/src/server/main/Main.java
+++ java/src/server/main/Main.java
@@ -1,14 +1,40 @@
 package server.main;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import static spark.Spark.*;
 
 /**
  * @author Derek Argueta
  */
-@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        get("/hello", (req, res) -> "Hello World!");
+
+        post("/user/login", (req, res) -> "Hello World!");
+        post("/user/register", (req, res) -> "Hello World!");
+
+        post("/games/create", (req, res) -> "Hello World!");
+        post("/games/join", (req, res) -> "Hello World!");
+
+        get("/game/model", (req, res) -> "Hello World!");
+        post("/game/addAI", (req, res) -> "Hello World!");
+        post("/game/listAI", (req, res) -> "Hello World!");
+
+        post("/moves/sendChat", (req, res) -> "Hello World!");
+        post("/moves/rollNumber", (req, res) -> "Hello World!");
+        post("/moves/robPlayer", (req, res) -> "Hello World!");
+        post("/moves/finishTurn", (req, res) -> "Hello World!");
+        post("/moves/buyDevCard", (req, res) -> "Hello World!");
+        post("/moves/Year_of_Plenty", (req, res) -> "Hello World!");
+        post("/moves/Road_Building", (req, res) -> "Hello World!");
+        post("/moves/Soldier", (req, res) -> "Hello World!");
+        post("/moves/Monopoly", (req, res) -> "Hello World!");
+        post("/moves/Monument", (req, res) -> "Hello World!");
+        post("/moves/buildRoad", (req, res) -> "Hello World!");
+        post("/moves/buildSettlement", (req, res) -> "Hello World!");
+        post("/moves/buildCity", (req, res) -> "Hello World!");
+        post("/moves/offerTrade", (req, res) -> "Hello World!");
+        post("/moves/acceptTrade", (req, res) -> "Hello World!");
+        post("/moves/maritimeTrade", (req, res) -> "Hello World!");
+        post("/moves/discardCards", (req, res) -> "Hello World!");
     }
 }
