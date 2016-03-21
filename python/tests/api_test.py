903ce20fd2eee6d318049788f824ae996300329c
Derek Argueta <darguetap@gmail.com>
2016-03-20 20:57:12 -0600

    move python scripts, and clone down requests library in case TA's don't have pip/virtualenv

--- python/tests/api_test.py
+++ python/tests/api_test.py
@@ -1,9 +1,13 @@
 
 import unittest
+import requests
+
 
 class LoginTests(unittest.TestCase):
     pass
 
 if __name__ == '__main__':
     print('ayyeee')
+    r = requests.get('https://www.google.com')
+    print(r.status_code)
     unittest.main()
