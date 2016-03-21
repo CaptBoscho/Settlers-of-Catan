533c4e075a911c7a594ec4141f45bd4d449c6e77
Derek Argueta <darguetap@gmail.com>
2016-03-21 04:07:40 -0600

    integration tests

--- python/tests/api_test.py
+++ python/tests/api_test.py
@@ -23,7 +23,9 @@ created_user_count = 0
 
 
 class JsonValidationTests(unittest.TestCase):
-    ''' Ensures that bad/malformed JSON returns a 400 error '''
+    ''' Ensures that bad/malformed JSON returns a 400 error. Only applicable
+        to POST endpoints.
+    '''
 
     def setUp(self):
         bad_payload = {
@@ -32,6 +34,7 @@ class JsonValidationTests(unittest.TestCase):
         self.bad_json = json.dumps(bad_payload)
         self.not_even_json = 'hi hello hey'
 
+    # -------- user
     def test_bad_register_json(self):
         r = requests.post('%suser/register' % BASE_URL, data=self.bad_json)
         self.assertEqual(requests.codes.bad_request, r.status_code)
@@ -58,6 +61,7 @@ class JsonValidationTests(unittest.TestCase):
         self.assertTrue('catan.user' not in r.cookies)
         self.assertTrue('catan.game' not in r.cookies)
 
+    # -------- games
     def test_bad_create_game_json(self):
         r = requests.post('%sgames/create' % BASE_URL, data=self.bad_json)
         self.assertEqual(requests.codes.bad_request, r.status_code)
@@ -85,9 +89,19 @@ class JsonValidationTests(unittest.TestCase):
         self.assertTrue('catan.game' not in r.cookies)
 
 
+class EndpointFilterTests(unittest.TestCase):
+    ''' Most endpoints have some kind of filter to either ensure the user is
+        authenticated or currenlty in a game. This test suite will ensure that
+        those filters are doing their job.
+    '''
+    # TODO
+    pass
+
+
 class HttpMethodTests(unittest.TestCase):
     ''' Validates that the endpoints only support the methods they should '''
 
+    # -------- user
     def test_GET_login(self):
         r = requests.get('%suser/login' % BASE_URL)
         self.assertEqual(requests.codes.not_found, r.status_code)
@@ -96,6 +110,147 @@ class HttpMethodTests(unittest.TestCase):
 
     def test_GET_register(self):
         r = requests.get('%suser/register' % BASE_URL)
+        self.assertEqual(requests.codes.not_found, r.status_code)
+        self.assertTrue('catan.user' not in r.cookies)
+        self.assertTrue('catan.game' not in r.cookies)
+
+    # -------- games
+    def test_POST_list_games(self):
+        r = requests.post('%sgames/list' % BASE_URL, data='')
+        self.assertEqual(requests.codes.not_found, r.status_code)
+        self.assertTrue('catan.user' not in r.cookies)
+        self.assertTrue('catan.game' not in r.cookies)
+
+    def test_GET_create_game(self):
+        r = requests.get('%sgames/create' % BASE_URL)
+        self.assertEqual(requests.codes.not_found, r.status_code)
+        self.assertTrue('catan.user' not in r.cookies)
+        self.assertTrue('catan.game' not in r.cookies)
+
+    def test_GET_join_game(self):
+        r = requests.get('%sgames/join' % BASE_URL)
+        self.assertEqual(requests.codes.not_found, r.status_code)
+        self.assertTrue('catan.user' not in r.cookies)
+        self.assertTrue('catan.game' not in r.cookies)
+
+    # -------- game
+    def test_POST_game_model(self):
+        r = requests.post('%sgame/model' % BASE_URL, data='')
+        self.assertEqual(requests.codes.not_found, r.status_code)
+        self.assertTrue('catan.user' not in r.cookies)
+        self.assertTrue('catan.game' not in r.cookies)
+
+    def test_GET_add_ai(self):
+        r = requests.get('%sgame/addAI' % BASE_URL)
+        self.assertEqual(requests.codes.not_found, r.status_code)
+        self.assertTrue('catan.user' not in r.cookies)
+        self.assertTrue('catan.game' not in r.cookies)
+
+    def test_POST_list_ai(self):
+        r = requests.post('%sgame/listAI' % BASE_URL, data='')
+        self.assertEqual(requests.codes.not_found, r.status_code)
+        self.assertTrue('catan.user' not in r.cookies)
+        self.assertTrue('catan.game' not in r.cookies)
+
+    # -------- moves
+    def test_GET_send_chat(self):
+        r = requests.get('%smoves/sendChat' % BASE_URL)
+        self.assertEqual(requests.codes.not_found, r.status_code)
+        self.assertTrue('catan.user' not in r.cookies)
+        self.assertTrue('catan.game' not in r.cookies)
+
+    def test_GET_roll_number(self):
+        r = requests.get('%smoves/rollNumber' % BASE_URL)
+        self.assertEqual(requests.codes.not_found, r.status_code)
+        self.assertTrue('catan.user' not in r.cookies)
+        self.assertTrue('catan.game' not in r.cookies)
+
+    def test_GET_rob_player(self):
+        r = requests.get('%smoves/robPlayer' % BASE_URL)
+        self.assertEqual(requests.codes.not_found, r.status_code)
+        self.assertTrue('catan.user' not in r.cookies)
+        self.assertTrue('catan.game' not in r.cookies)
+
+    def test_GET_finish_turn(self):
+        r = requests.get('%smoves/finishTurn' % BASE_URL)
+        self.assertEqual(requests.codes.not_found, r.status_code)
+        self.assertTrue('catan.user' not in r.cookies)
+        self.assertTrue('catan.game' not in r.cookies)
+
+    def test_GET_buy_dev_card(self):
+        r = requests.get('%smoves/buyDevCard' % BASE_URL)
+        self.assertEqual(requests.codes.not_found, r.status_code)
+        self.assertTrue('catan.user' not in r.cookies)
+        self.assertTrue('catan.game' not in r.cookies)
+
+    def test_GET_year_of_plenty(self):
+        r = requests.get('%smoves/Year_of_Plenty' % BASE_URL)
+        self.assertEqual(requests.codes.not_found, r.status_code)
+        self.assertTrue('catan.user' not in r.cookies)
+        self.assertTrue('catan.game' not in r.cookies)
+
+    def test_GET_road_building(self):
+        r = requests.get('%smoves/Road_Building' % BASE_URL)
+        self.assertEqual(requests.codes.not_found, r.status_code)
+        self.assertTrue('catan.user' not in r.cookies)
+        self.assertTrue('catan.game' not in r.cookies)
+
+    def test_GET_soldier(self):
+        r = requests.get('%smoves/Soldier' % BASE_URL)
+        self.assertEqual(requests.codes.not_found, r.status_code)
+        self.assertTrue('catan.user' not in r.cookies)
+        self.assertTrue('catan.game' not in r.cookies)
+
+    def test_GET_monopoly(self):
+        r = requests.get('%smoves/Monopoly' % BASE_URL)
+        self.assertEqual(requests.codes.not_found, r.status_code)
+        self.assertTrue('catan.user' not in r.cookies)
+        self.assertTrue('catan.game' not in r.cookies)
+
+    def test_GET_monument(self):
+        r = requests.get('%smoves/Monument' % BASE_URL)
+        self.assertEqual(requests.codes.not_found, r.status_code)
+        self.assertTrue('catan.user' not in r.cookies)
+        self.assertTrue('catan.game' not in r.cookies)
+
+    def test_GET_build_road(self):
+        r = requests.get('%smoves/buildRoad' % BASE_URL)
+        self.assertEqual(requests.codes.not_found, r.status_code)
+        self.assertTrue('catan.user' not in r.cookies)
+        self.assertTrue('catan.game' not in r.cookies)
+
+    def test_GET_build_settlement(self):
+        r = requests.get('%smoves/buildSettlement' % BASE_URL)
+        self.assertEqual(requests.codes.not_found, r.status_code)
+        self.assertTrue('catan.user' not in r.cookies)
+        self.assertTrue('catan.game' not in r.cookies)
+
+    def test_GET_build_city(self):
+        r = requests.get('%smoves/buildCity' % BASE_URL)
+        self.assertEqual(requests.codes.not_found, r.status_code)
+        self.assertTrue('catan.user' not in r.cookies)
+        self.assertTrue('catan.game' not in r.cookies)
+
+    def test_GET_offer_trade(self):
+        r = requests.get('%smoves/offerTrade' % BASE_URL)
+        self.assertEqual(requests.codes.not_found, r.status_code)
+        self.assertTrue('catan.user' not in r.cookies)
+        self.assertTrue('catan.game' not in r.cookies)
+
+    def test_GET_accept_trade(self):
+        r = requests.get('%smoves/acceptTrade' % BASE_URL)
+        self.assertEqual(requests.codes.not_found, r.status_code)
+        self.assertTrue('catan.user' not in r.cookies)
+        self.assertTrue('catan.game' not in r.cookies)
+
+    def test_GET_maritime_trade(self):
+        r = requests.get('%smoves/maritimeTrade' % BASE_URL)
+        self.assertEqual(requests.codes.not_found, r.status_code)
+        self.assertTrue('catan.user' not in r.cookies)
+        self.assertTrue('catan.game' not in r.cookies)
+
+    def test_GET_discard_cards(self):
+        r = requests.get('%smoves/discardCards' % BASE_URL)
         self.assertEqual(requests.codes.not_found, r.status_code)
         self.assertTrue('catan.user' not in r.cookies)
         self.assertTrue('catan.game' not in r.cookies)
