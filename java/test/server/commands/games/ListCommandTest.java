4489eaf1514e6d7c858149471d901f5f9d8475a9
Kyle Cornelison <cornelison.kylejohn@gmail.com>
2016-03-23 19:53:13 -0600

    Done - minus the mock server facade

--- java/test/server/commands/games/ListCommandTest.java
+++ java/test/server/commands/games/ListCommandTest.java
@@ -3,6 +3,8 @@ package server.commands.games;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
+import server.commands.CommandExecutionResult;
 import server.facade.MockFacade;
 import server.main.Config;
 
@@ -21,5 +23,15 @@ public class ListCommandTest {
     @After
     public void tearDown() {
 
+    }
+
+    /**
+     * Validates that the command works - this is a simple list command
+     */
+    @Test
+    public void testExecute() {
+        CommandExecutionResult result = new ListCommand().execute();
+        assertTrue(result != null);
+        assertFalse(result.errorOccurred());
     }
 }
