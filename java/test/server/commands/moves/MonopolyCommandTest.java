056ca151a585cdf59681b5e147666fcd098202f6
Kyle Cornelison <cornelison.kylejohn@gmail.com>
2016-03-23 19:38:04 -0600

    Done - minus the mock server facade

--- java/test/server/commands/moves/MonopolyCommandTest.java
+++ java/test/server/commands/moves/MonopolyCommandTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import server.exceptions.CommandExecutionFailedException;
 import server.facade.MockFacade;
 import server.main.Config;
+import shared.dto.CookieWrapperDTO;
+import shared.dto.PlayMonopolyDTO;
 
 /**
  * Unit Testing for the "Play Monopoly" command.
@@ -41,5 +43,16 @@ public class MonopolyCommandTest {
     @Test(expected = AssertionError.class)
     public void testExecuteWithMissingParams() throws CommandExecutionFailedException {
         new MonopolyCommand().execute();
+    }
+
+    /**
+     * Validates that the command works with valid parameters
+     */
+    @Test
+    public void testExecuteWithValidParams() throws CommandExecutionFailedException {
+        MonopolyCommand monopolyCommand = new MonopolyCommand();
+        CookieWrapperDTO wrapperDTO = new CookieWrapperDTO(new PlayMonopolyDTO(0, "wood"));
+        monopolyCommand.setParams(wrapperDTO);
+        monopolyCommand.execute();
     }
 }
