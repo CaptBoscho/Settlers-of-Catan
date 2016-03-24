db398b68215855063f697e863e7b24d6eb700a2e
Derek Argueta <darguetap@gmail.com>
2016-03-23 19:46:06 -0600

    can set host/port

--- java/src/server/main/Main.java
+++ java/src/server/main/Main.java
@@ -16,6 +16,8 @@ import server.handlers.moves.*;
 import server.managers.GameManager;
 import server.managers.UserManager;
 
+import java.util.Arrays;
+
 import static spark.Spark.*;
 
 /**
@@ -38,7 +40,7 @@ public class Main {
         }
 
         // for now, hardcode to port 8081
-        port(8081);
+        port(Config.port);
 
         // the following endpoint patterns require authentication cookies
 //        before("/games/*", new AuthenticationFilter()); TODO this is literally the worst application design that we are being forced to adhere to. And Swagger sucks kthnxbai.
