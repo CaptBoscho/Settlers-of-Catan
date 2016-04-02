e6b6027fc397958decde9bfe168db74c838ef71e
Kyle Cornelison <cornelison.kylejohn@gmail.com>
2016-04-02 15:28:58 -0600

    More work on Persistence
    Added the IDAO Interface - Corbin is stubbing the DAOs
    Added Exceptions
    Stubbed the IPersistenceProvider Interface

--- java/src/server/persistence/IPersistenceProvider.java
+++ java/src/server/persistence/IPersistenceProvider.java
@@ -1,8 +1,55 @@
 package server.persistence;
 
+import server.exceptions.EndTransactionException;
+import server.exceptions.StartTransactionException;
+import server.persistence.dao.IDAO;
+
 /**
  * Created by Kyle 'TMD' Cornelison on 4/2/2016.
  */
 public interface IPersistenceProvider {
-    
+    //region Plugin Methods
+    /**
+     * Clears the database
+     */
+    void clear();
+
+    /**
+     * Starts a transaction on the database
+     *
+     * @throws StartTransactionException
+     */
+    void startTransaction() throws StartTransactionException;
+
+    /**
+     * Ends a transaction on the database
+     *
+     * @param commitTransaction
+     * @throws EndTransactionException
+     */
+    void endTransaction(boolean commitTransaction) throws EndTransactionException;
+    //endregion
+
+    //region Factory Methods
+    /**
+     * Creates and returns a new UserDAO
+     *
+     * @return UserDAO which implements IDAO interface
+     */
+    IDAO getUserDAO();
+
+    /**
+     * Creates and returns a new GameDAO
+     *
+     * @return GameDAO which implements IDAO interface
+     */
+    IDAO getGameDAO();
+
+    /**
+     * Creates and returns a new CommandDAO
+     *
+     * @return CommandDAO which implements IDAO interface
+     */
+    IDAO getCommandDAO();
+    //endregion
 }
