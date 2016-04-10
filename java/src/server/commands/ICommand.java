46d5eb1a1259f867fd7ae5d6a29956cdd540c11d
Derek Argueta <darguetap@gmail.com>
2016-04-10 16:38:07 -0600

    tweak

--- java/src/server/commands/ICommand.java
+++ java/src/server/commands/ICommand.java
@@ -3,13 +3,15 @@ package server.commands;
 import server.exceptions.CommandExecutionFailedException;
 import shared.dto.IDTO;
 
+import java.io.Serializable;
+
 /**
  * A parent interface for all command objects.  The execute function in each implementation of this interface will
  * carry out a task in the server to get the desires information.
  *
  * @author Danny Harding
  */
-public interface ICommand {
+public interface ICommand extends Serializable {
 
     /**
      * The main function on child classes of this interface.  It can be called to activate the set of instructions
